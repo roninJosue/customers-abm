2a0737d46cd25cc821d9bf07b9a529f2a12f3b99
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-01-25 21:01:25 -0600

    Applying routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import {Link, BrowserRouter as Router} from "react-router-dom";
+import {Link, BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
+  const home = () => <h1>Home</h1>
+  const customers = () => <h1>Customers</h1>
+  const customerDni = () => <h1>Customer DNI</h1>
+  const customerNew = () => <h1>Customer NEW</h1>
+  const notFound = () => <h1>Not Found</h1>
+
   return (
     <Router>
-      <div className="App">
-        <Link to='/customers'>Customers</Link>
-        <Link to='/customers/3000000'>Customers 3000</Link>
-      </div>
+      <Routes>
+        <Route path='/' element={home()}/>
+        <Route path='/customers' element={customers()}/>
+        <Route path='/customers/:dni' element={customerDni()}/>
+        <Route path='/customers/new' element={customerNew()}/>
+        <Route path='*' element={notFound()} />
+      </Routes>
     </Router>
   );
 }
---
+++ src/components/CustomersList.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CustomerListItem from "./CustomerListItem";
+
+const CustomersList = ({customers, urlPath}) => {
+  return (
+    <div>
+      <div className="customers-list">
+        {customers.map((customer) => (
+          <CustomerListItem
+            key={customer.dni}
+            urlPath={urlPath}
+            editAction='Edit'
+            name={customer.name}
+            delAction='Delete'
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+CustomersList.propTypes = {
+  customers: PropTypes.array.isRequired,
+  urlPath: PropTypes.string.isRequired
+};
+
+export default CustomersList;
